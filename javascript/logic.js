7675801bdfd9bc8e76587d9dcf6dbaa2169bfb5e
andyreesecups <andyreesecups@yahoo.com>
2017-06-19 13:08:48 -0500

    Added tetris button functionality

--- javascript/logic.js
+++ javascript/logic.js
@@ -10,6 +10,27 @@ const matrix = [
     [0, 1, 0],
 ];
 
+function collide(arena, player) {
+	const [m, o] = [player.matrix, player.pos];
+	for(let y = 0; y < m.length; ++y) {
+		for(let x = 0; x < m[y].length; ++x) {
+			if(m[y][x] !== 0 && 
+			(arena [y + o.y] && arena [y + o.y][x + o.x]) !== 0) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+function createMatrix(w, h) {
+	const matrix = [];
+	while(h--) {
+		matrix.push(new Array(w).fill(0));
+	}
+	return matrix;
+}
+
 function draw() {
 	context.fillStyle = '#000';
 context.fillRect(0, 0, canvas.width, canvas.height);
@@ -27,15 +48,63 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function merge(arena, player) {
+	player.matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if (value !== 0) {
+				arena[y + player.pos.y][x + player.pos.x] = value;
+			}
+		});
+	});
+}
+
+// This function drop the tetris down when the down button is clicked
+function playerDrop() {
+	player.pos.y++;
+	if(collide(arena, player)) {
+		player.pos.y--;
+		merge(arena, player);
+		player.pos.y = 0;
+	}
+	dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
 function update(time = 0) {
+	const deltaTime = time - lastTime;
+	lastTime = time;
+	dropCounter += deltaTime;
+	if(dropCounter > dropInterval) {
+		playerDrop();
+	}
+
 	draw();
 	requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
+
 
 const player = {
 	pos: {x:5, y:5},
 	matrix: matrix, 
 }
+
+document.addEventListener('keydown', event => {
+	// if player presses left button on keyboard then move the tetris block to the left
+	if(event.keyCode === 37) {
+		player.pos.x--;
+		// else if player presses right button then move the tetris to the right
+	} else if (event.keyCode === 39) {
+		player.pos.x++;
+		// else if invoke playerDrop function
+	} else if (event.keyCode === 40) {
+		playerDrop();
+	}
+});
 
 update();
 
